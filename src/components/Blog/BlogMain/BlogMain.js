c1fed93cda617618b30629a1316a6b9fd8df03ea
Diego Nascimento <diego.nascimento@liferay.com>
2018-10-19 14:38:38 -0300

    Adds BlogMain component

---
+++ src/components/Blog/BlogMain/BlogMain.js
@@ -0,0 +1,27 @@
+import { graphql } from 'gatsby';
+import React, { Component } from 'react';
+import CodeTabs from '../../CodeTabs';
+import CodeClipboard from '../../CodeClipboard';
+import BlogList from '../BlogList';
+
+export default class BlogMain extends Component {
+    componentDidMount() {
+        this._codeTabs = new CodeTabs();
+        this._codeClipboard = new CodeClipboard();
+    }
+
+    componentWillUnmount() {
+        this._codeTabs = null;
+        this._codeClipboard.dispose();
+    }
+
+    render() {
+        return (
+            <>
+                <h1> {this.props.title ? this.props.title : "Blog" } </h1>
+
+                <BlogList />
+            </>
+        );
+    }
+}
