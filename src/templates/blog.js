792445f5a56745d43bd78ad73c5ac4fc3d9585b7
Diego Nascimento <diego.nascimento@liferay.com>
2018-10-19 14:38:20 -0300

    Use BlogList and BlogMain components on Blog template

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -3,10 +3,10 @@ import Helmet from 'react-helmet';
 import MDXRenderer from 'gatsby-mdx/mdx-renderer';
 import React, { Component } from 'react';
 import SimpleFooter from '../components/SimpleFooter'
-import Sidebar from '../components/Sidebar';
 import CodeTabs from '../components/CodeTabs';
 import CodeClipboard from '../components/CodeClipboard';
 import Typography from '../components/Typography';
+import BlogMain from '../components/Blog/BlogMain';
 
 export default class Blog extends Component {
     componentDidMount() {
@@ -21,9 +21,7 @@ export default class Blog extends Component {
 
     render() {
         const { data } = this.props;
-        const { mdx: { code, frontmatter, excerpt, timeToRead } } = data;
-
-        const title = `${frontmatter.title} - Clay`;
+        const { mdx: { code, frontmatter: { title, mainPage }, excerpt, timeToRead } } = data;
 
         const githubRepo = "https://github.com/liferay/clay";
 
@@ -45,53 +43,37 @@ export default class Blog extends Component {
                 <main className="content">
                     <header>
                         <div className="clay-site-container container-fluid">
-                            <h1>{frontmatter.title}</h1>
+                            <h1>{title}</h1>
                         </div>
                     </header>
-
                     <div className="clay-site-container container-fluid">
                         <div className="row">
                             <div className="col-md-12">
                                 <article>
-                                    <MDXRenderer
-                                        components={{
-                                            h1: Typography.H1,
-                                            h2: Typography.H2,
-                                            h3: Typography.H3,
-                                            h4: Typography.H4,
-                                            p: Typography.P,
-                                        }}
-                                    >
-                                        {code.body}
-                                    </MDXRenderer>
+
+                                    {/* renders the main page */}
+                                    {mainPage &&
+                                        <BlogMain title={title} excerpt={excerpt} timeToRead={timeToRead} />
+                                    }
+
+                                    {/*renders a blog post content */}
+                                    {!mainPage &&
+                                        <MDXRenderer
+                                            components={{
+                                                h1: Typography.H1,
+                                                h2: Typography.H2,
+                                                h3: Typography.H3,
+                                                h4: Typography.H4,
+                                                p: Typography.P,
+                                            }}
+                                        >
+                                            {code.body}
+                                        </MDXRenderer>
+                                    }
                                 </article>
                             </div>
                         </div>
                     </div>
-                    {/* {posts
-                        .map(({ node: post }) => (
-                            <div
-                                className="content"
-                                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                                key={post.id}
-                            >
-                                <p>
-                                    <Link className="has-text-primary" to={post.fields.slug}>
-                                        {post.frontmatter.title}
-                                    </Link>
-                                    <span> &bull; </span>
-                                    <small>{post.frontmatter.date}</small>
-                                </p>
-                                <p>
-                                    {post.excerpt}
-                                    <br />
-                                    <br />
-                                    <Link className="button is-small" to={post.fields.slug}>
-                                        Keep Reading →
-                                    </Link>
-                                </p>
-                            </div>
-                    ))} */}
                 </main>
 
                 <SimpleFooter githubRepo={githubRepo} />
@@ -108,8 +90,7 @@ export const pageQuery = graphql`
             timeToRead
             frontmatter {
                 title
-                date(formatString: "MMMM DD, YYYY")
-                description
+                mainPage
             }
             code {
                 body
