75fb9dc15b3a65c82dd43ba840e6132617cbb3ea
Diego Nascimento <diego.nascimento@liferay.com>
2018-10-24 17:13:45 -0300

    Turn Boilerplate as true PWA

--- src/html.js
+++ src/html.js
@@ -15,7 +15,6 @@ class HTML extends Component {
                     <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png" />
-                    <link rel="manifest" href="/images/favicon/site.webmanifest" />
                     <link rel="mask-icon" href="/images/favicon/safari-pinned-tab.svg" color="#0b5fff" />
                     <meta name="msapplication-TileColor" content="#ffffff" />
                     <meta name="theme-color" content="#ffffff" />
