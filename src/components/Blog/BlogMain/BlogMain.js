2bc537c9e6b63ed25216154c009810d30d0f1bca
Diego Nascimento <diego.nascimento@liferay.com>
2018-10-24 15:12:45 -0300

    Refactor Blog files

--- src/components/Blog/BlogMain/BlogMain.js
+++ src/components/Blog/BlogMain/BlogMain.js
@@ -1,21 +1,7 @@
-import { graphql } from 'gatsby';
 import React, { Component } from 'react';
-import CodeTabs from '../../CodeTabs';
-import CodeClipboard from '../../CodeClipboard';
 import BlogList from '../BlogList';
-import LayoutNav from '../../LayoutNav';
 
 export default class BlogMain extends Component {
-    componentDidMount() {
-        this._codeTabs = new CodeTabs();
-        this._codeClipboard = new CodeClipboard();
-    }
-
-    componentWillUnmount() {
-        this._codeTabs = null;
-        this._codeClipboard.dispose();
-    }
-
     render() {
         return (
             <>
