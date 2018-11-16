049c78a76a251be1387a737180bd3331a5043b7c
Diego Nascimento <diego.nascimento@liferay.com>
2018-11-15 23:13:09 -0300

    Redesign Index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ class Index extends Component {
     }
 
     render() {
-        const description = "Make powerful static sites 💖";
+        const description = "Start using Gatsby for make powerful static sites 💖";
 
         return (
             <div className="home">
@@ -43,14 +43,8 @@ class Index extends Component {
                                         <Link to="/docs/" className="btn btn-lg btn-outline-light font-weight-bold mx-3 mb-4">
                                             Docs
                                         </Link>
-                                        <Link to="/blog/" className="btn btn-lg btn-outline-light font-weight-bold mx-3 mb-4">
-                                            Blog
-                                        </Link>
                                         <Link to="/onboarding/" className="btn btn-lg btn-outline-light font-weight-bold mx-3 mb-4">
-                                            Onboarding
-                                        </Link>
-                                        <Link to="/updates/" className="btn btn-lg btn-outline-light font-weight-bold mx-3 mb-4">
-                                            Updates
+                                            Get Started
                                         </Link>
                                     </div>
                                 </div>
@@ -74,45 +68,6 @@ class Index extends Component {
                             </div>
                         </div>
                     </section>
-
-                    <section className="link-blocks text-center">
-                        <div className="container-fluid container-fluid-max-lg">
-                            <div className="row">
-                                <div className="body ml-auto col-md-9 mr-auto">
-                                    <h2 className="h1 font-weight-bold">This is a amazing boilerplate</h2>
-                                    <p className="pb-5 pt-4">This boilerplate uses Gatsby! <div className="font-weight-bold">So fast, wow</div></p>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-md-5 ml-auto mb-4">
-                                    <div className="card h-100 wow fadeInUp">
-                                        <div className="card-body mx-5">
-                                            <img className="mx-auto" src="/images/home/liferay-logo-full-color.svg" alt="" />
-                                            <p>Check this card, wow <br />
-                                            Awesome! Check this incredible company website!</p>
-                                            <a href="https://liferay.com" className="mb-3"  target="_blank" rel="noopener noreferrer">
-                                                Visit Liferay website
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-md-5 mr-auto mb-4">
-                                    <div className="card h-100 wow fadeInUp" data-wow-delay="0.1s">
-                                        <div className="card-body mx-5">
-                                            <img className="mx-auto" src="/images/home/gatsby.svg" alt="" />
-                                            <p>
-                                                Cambrian explosion citizens of distant epochs invent the universe dispassionate extraterrestrial observer birth extraplanetary?
-                                            </p>
-                                            <a href="https://gatsbyjs.org" className="mb-3"  target="_blank" rel="noopener noreferrer">
-                                                Visit Gatsby Page
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-
                     <section>
                         <Footer />
                     </section>
